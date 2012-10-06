29ef278ea1655335ba04803283cf5cec54db9945
Jan-Erik Rediger <badboy@archlinux.us>
2012-10-06 18:30:44 +0200

    New resource Recurring implemented.

--- lib/badbill.rb
+++ lib/badbill.rb
@@ -12,6 +12,7 @@ require_relative 'badbill/client'
 require_relative 'badbill/invoice'
 require_relative 'badbill/invoice_payment'
 require_relative 'badbill/invoice_item'
+require_relative 'badbill/recurring'
 
 # Handles the connection and requests to the Billomat API.
 #
@@ -27,7 +28,7 @@ require_relative 'badbill/invoice_item'
 #     billo.get 'clients'
 #     # => {"clients"=>{"client"=>[...]}}
 class BadBill
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 
   # Reject any not allowed HTTP method.
   class NotAllowedException < Exception; end
